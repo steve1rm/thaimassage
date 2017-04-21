ae7ceabf692a440ee4ac90277ff51e48c2b9dffe
Steve Mason <steve@androidbox.me>
2017-04-22 01:38:17 +0700

    Added simple login activity

---
+++ app/src/main/java/me/androidbox/thaimassage/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package me.androidbox.thaimassage.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.androidbox.thaimassage.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/me/androidbox/thaimassage/login/LoginView.java
@@ -0,0 +1,32 @@
+package me.androidbox.thaimassage.login;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.androidbox.thaimassage.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginView extends Fragment {
+
+
+    public LoginView() {
+        // Required empty public constructor
+    }
+
+    public static LoginView newInstance() {
+        return new LoginView();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.login_view, container, false);
+    }
+}
